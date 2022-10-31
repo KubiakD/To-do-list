9888b45122cadd296b97a7e058eb0a77a115ee2c
Daniel Kubiak <kubiakdaniel97@gmail.com>
2022-11-01 00:08:24 +0100

    add deleting tasks while they are done

--- scripts/items.js
+++ scripts/items.js
@@ -35,40 +35,36 @@ function createNewItem() {
   const listItem = document.createElement('li');
   const collapseTitleDiv = document.createElement('div');
   const iconsDiv = document.createElement('div');
-  // const deleteSpan = document.createElement('span');
   const checkboxItem = document.createElement('input');
   const expandSpan = document.createElement('span');
   const highPrioritySpan = document.createElement('span');
   const collapseContentDiv = document.createElement('div');
-
+  
   checkboxItem.type = 'checkbox';
 
   collapseTitleDiv.classList.add('collapse-title');
   iconsDiv.classList.add('icons');
   highPrioritySpan.classList.add('material-symbols-outlined', 'high-priority');
-  // deleteSpan.classList.add('material-symbols-outlined', 'delete');
   checkboxItem.classList.add('material-symbols-outlined', 'checkbox');
   expandSpan.classList.add('material-symbols-outlined', 'expand');
   collapseContentDiv.classList.add('collapse-content');
-
-  // deleteSpan.innerText = 'delete';
+  
   expandSpan.innerText = 'expand_more';
   highPrioritySpan.innerText = 'priority_high';
   const newItemData = getLastItem();
   collapseTitleDiv.innerText = newItemData.title;
   collapseContentDiv.innerText = newItemData.note;
   listItem.dataset.id = newItemData.Id;
-
+  
   if (newItemData.isUrgent) {
     iconsDiv.appendChild(highPrioritySpan);
   }
-  // iconsDiv.appendChild(deleteSpan);
   iconsDiv.appendChild(checkboxItem);
   iconsDiv.appendChild(expandSpan);
   listItem.appendChild(collapseTitleDiv);
   collapseTitleDiv.appendChild(iconsDiv);
   listItem.appendChild(collapseContentDiv);
-
+  
   if (!newItemData.isUrgent) {
     nonUrgentTasksList.appendChild(listItem);
   } else {
@@ -92,39 +88,49 @@ function addEventListeners() {
 function expandTitle(event) {
   const selectedButton = event.target;
   const selectedElement =
-    selectedButton.parentElement.parentElement.parentElement;
-
+  selectedButton.parentElement.parentElement.parentElement;
+  
   selectedElement.classList.toggle('active');
   selectedButton.classList.toggle('activeIcon');
 }
 
 function assignToList(event) {
-  const selectedElement =
-    event.target.parentElement.parentElement.parentElement;
+  const selectedElementIconDiv = event.target.parentElement;
+  const selectedElement = event.target.parentElement.parentElement.parentElement;
   const selectedElementId = selectedElement.dataset.id;
   const selectedElementData = items[selectedElementId - 1];
-
+  
   if (!doneTasksList.contains(selectedElement)) {
+    addDeleteIcon(selectedElementIconDiv);
     doneTasksList.appendChild(selectedElement);
   } else if (
     doneTasksList.contains(selectedElement) &&
     selectedElementData.isUrgent
   ) {
     urgentTasksList.appendChild(selectedElement);
+    selectedElementIconDiv.lastChild.remove();
   } else if (
     doneTasksList.contains(selectedElement) &&
     !selectedElementData.isUrgent
   ) {
     nonUrgentTasksList.appendChild(selectedElement);
+    selectedElementIconDiv.lastChild.remove();
   } else {
     console.log('Error!');
-  }
+  };  
 }
-
+    
+function addDeleteIcon(taskItem) {
+  const deleteSpan = document.createElement('span');
+      
+  deleteSpan.classList.add('material-symbols-outlined', 'delete');
+  deleteSpan.innerText = 'delete';
+  taskItem.appendChild(deleteSpan);  
+  deleteSpan.addEventListener('click', removeElement);
+};
+    
 function removeElement(event) {
   const selectedButton = event.target;
-  const selectedElement =
-    selectedButton.parentElement.parentElement.parentElement;
-
+  const selectedElement = selectedButton.parentElement.parentElement.parentElement;    
   selectedElement.remove();
-}
+    }
